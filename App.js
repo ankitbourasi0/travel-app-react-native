2f56adcd6b63c5cf2737a256ae59ff7f8c488c96
Ankit Bourasi <ankitbourasi0@gmail.com>
2022-06-26 01:19:31 +0530

    screen-wrape

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ScreenWrapper from './components/screenWrapper'
+
+const App = () => {
+  return (
+    <ScreenWrapper>
+    <View>
+      <Text>App</Text>
+    </View>
+    </ScreenWrapper>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
